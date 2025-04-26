0b760919b8b2405c0739c6f49b7c47f8e35aa56d
Manitra <94074919+ShibaMiyuki07@users.noreply.github.com>
2025-04-26 10:02:04 +0300

    Refactorize the video downloader

--- Services/VideoDownloaderService.cs
+++ Services/VideoDownloaderService.cs
@@ -12,47 +12,35 @@ using YoutubeExplode.Videos.Streams;
 using ClipboardUrl.Utils;
 using System.IO;
 using System.Threading;
-using Id3;
+using Xabe.FFmpeg;
 
 namespace ClipboardUrl.Services
 {
     public class VideoDownloaderService : IDownload
     {
-        CancellationToken token = new CancellationToken();
         public async Task Download(string url)
         {
-            var video = await Const.youtubeClient.Videos.GetAsync(url);
-            var streamInfo = await Const.youtubeClient.Videos.Streams.GetManifestAsync(url);
-            var audio = streamInfo.GetAudioOnlyStreams().GetWithHighestBitrate();
+
+            (var video, var audio) = await VideoInfo.GetInfo(url);
 
 
-            DownloadFile downloadFile = DownloadFile.initializeDownloadFile(video);
+            DownloadFile downloadFile = DownloadFile.InitializeDownloadFile(video);
 
+            string audioFullPath = Path.Combine(Const.path, $@"{downloadFile.Title}.{audio.Container.Name}");
 
-            if(!Directory.Exists(Const.path))
-                Directory.CreateDirectory(Const.path);
-            string fullpath = Path.Combine(Const.path, $@"{downloadFile.Title}.mp3");
-            if(!File.Exists(fullpath))
-            {
-                using (var audioStream = File.Create(fullpath))
-                {
-                    await Const.youtubeClient.Videos.Streams.CopyToAsync(audio, audioStream, cancellationToken: token);
-                }
-                using (var mp3 = new Mp3(fullpath,Mp3Permissions.ReadWrite))
-                {
-                    Id3Tag tag = mp3.GetTag(Id3TagFamily.Version2X);
-                    if(tag == null)
-                    {
-                        tag = new Id3Tag();
-                    }
-                    tag.Title = downloadFile.Title;
-                    tag.Publisher = downloadFile.Author;
-                    mp3.WriteTag(tag, WriteConflictAction.Replace);
-                }
-            }
-            
+            await DownloadAudio(audioFullPath, audio);
+
+            Console.WriteLine("Processing mp3 conversion");
+            await AudioConverter.ConvertAudioToMp3(audioFullPath, downloadFile);
+            Console.WriteLine("Mp3 conversion finished");
+            FileService.DeleteFile(audioFullPath);
         }
         
+        private async Task DownloadAudio(string audioFullPath,IStreamInfo audio)
+        {
+            await Const.youtubeClient.Videos.Streams.DownloadAsync(audio, audioFullPath);
+        }
 
+        
     }
 }
